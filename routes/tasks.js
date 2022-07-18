17289de32b5a331bf56afe226f0d34264606c9ba
Juventino Neto <nettojuventino@gmail.com>
2022-07-18 13:58:01 -0300

    New endpoints

--- routes/tasks.js
+++ routes/tasks.js
@@ -18,9 +18,29 @@ router.get('/:year/:month', (req, res, next) => {
     res.status(200).json('List the tasks of a specific year and month...');
 });
 
-// GET tasks/2022
+// GET tasks/2022/12/12
 router.get('/:year/:month/:day', (req, res, next) => {
     res.status(200).json(new TaskModel('First task', '2022-07-17', Status.waiting));
+});
+
+// POST
+router.post('/', (req, res, next) => {
+    res.status(201).json({ 'sucess': true, 'message': 'New task created' });
+});
+
+// PATH
+router.patch('/:id/change-status', (req, res, next) => {
+    res.status(200).json({ 'sucess': true, 'message': 'The status has been changed' });
+});
+
+// PUT
+router.put('/:id', (req, res, next) => {
+    res.status(200).json({ 'sucess': true, 'message': 'Task edited' });
+});
+
+// DELETE
+router.delete('/:id', (req, res, next) => {
+    res.status(200).json({ 'sucess': true, 'message': 'Task excluded' });
 });
 
 module.exports = router;
